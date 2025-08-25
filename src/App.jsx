5ebcbf46977c7f91eb6fbadc77eeb3ec3c70c121
SanjayC-19 <sanjaycs1902@gmail.com>
2025-08-25 14:31:41 +0530

    new pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
-import AboutPage from "./components/AboutPage";
+import About from "./components/About";
+import Careers from "./components/career";
+import Events from "./components/Events";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Product from "./components/Product";
+import Search from "./components/Search";
+import Wishlist from "./components/Wishlist";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/about" element={<AboutPage />} />
+        <Route path="/home" element={<Navbar />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<Careers />} />
+        <Route path="/about" element={<Events />} />
+        <Route path="/about" element={<Product />} />
+        <Route path="/about" element={<Search />} />
+        <Route path="/about" element={<Wishlist />} />
+        <Route path="/about" element={<Footer />} />
       </Routes>
     </Router>
   );
